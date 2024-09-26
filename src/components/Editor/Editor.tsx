baa84d610fb6cb092fa7a994e1d10552ce7c1112
hunghg255 <giahunghust@gmail.com>
2024-09-27 00:26:44 +0700

    feat: add attachment

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -48,9 +48,24 @@ import RcTiptapEditor, {
   TableOfContents,
   Excalidraw,
   TextDirection,
+  Mention,
+  Attachment
 } from 'reactjs-tiptap-editor';
 
 import 'reactjs-tiptap-editor/style.css';
+
+function convertBase64ToBlob(base64: string) {
+  const arr = base64.split(',')
+  const mime = arr[0].match(/:(.*?);/)![1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new Blob([u8arr], { type: mime })
+}
+
 
 const extensions = [
   BaseKit.configure({
@@ -152,6 +167,21 @@ const extensions = [
   }),
   ExportWord,
   Excalidraw,
+  Mention,
+  Attachment.configure({
+    upload: (file: any) => {
+      // fake upload return base 64
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const blob = convertBase64ToBlob(reader.result as string)
+          resolve(URL.createObjectURL(blob))
+        }, 300)
+      })
+    },
+  }),
 ];
 
 const DEFAULT = `<h1 style="text-align: center">Rich Text Editor</h1><p>A modern WYSIWYG rich text editor based on <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://github.com/scrumpy/tiptap">tiptap</a> and <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://ui.shadcn.com/">shadcn ui</a> for Reactjs</p><p></p><p style="text-align: center"></p><div style="text-align: center;" class="image"><img height="auto" src="https://picsum.photos/1920/1080.webp?t=1" align="center" width="500"></div><p></p><div data-type="horizontalRule"><hr></div><h2>Demo</h2><p>👉<a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://reactjs-tiptap-editor.vercel.app/">Demo</a></p><h2>Features</h2><ul><li><p>Use <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://ui.shadcn.com/">shadcn ui</a> components</p></li><li><p>Markdown support</p></li><li><p>TypeScript support</p></li><li><p>I18n support (vi, en, zh, pt)</p></li><li><p>React support</p></li><li><p>Slash Commands</p></li><li><p>Multi Column</p></li><li><p>TailwindCss</p></li><li><p>Support emoji</p></li><li><p>Support iframe</p></li></ul><h2>Installation</h2><pre><code class="language-bash">pnpm add reactjs-tiptap-editor</code></pre><p></p>`;
