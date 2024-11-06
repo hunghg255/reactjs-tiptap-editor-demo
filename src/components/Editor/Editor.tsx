c0efc51488ba262c299cf2aac4b684bcd63dff4c
hunghg255 <giahunghust@gmail.com>
2024-11-06 18:07:42 +0700

    feat: add extension twitter

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -49,7 +49,8 @@ import RcTiptapEditor, {
   Mention,
   Attachment,
   ImageGif,
-  Mermaid
+  Mermaid,
+  Twitter
 } from 'reactjs-tiptap-editor';
 
 import 'reactjs-tiptap-editor/style.css';
@@ -179,6 +180,7 @@ const extensions = [
       })
     },
   }),
+  Twitter,
 ];
 
 const DEFAULT = `<h1 style="text-align: center">Rich Text Editor</h1><p>A modern WYSIWYG rich text editor based on <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://github.com/scrumpy/tiptap">tiptap</a> and <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://ui.shadcn.com/">shadcn ui</a> for Reactjs</p><p></p><p style="text-align: center"></p><div style="text-align: center;" class="image"><img height="auto" src="https://picsum.photos/1920/1080.webp?t=1" align="center" width="500"></div><p></p><div data-type="horizontalRule"><hr></div><h2>Demo</h2><p>👉<a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://reactjs-tiptap-editor.vercel.app/">Demo</a></p><h2>Features</h2><ul><li><p>Use <a target="_blank" rel="noopener noreferrer nofollow" class="link" href="https://ui.shadcn.com/">shadcn ui</a> components</p></li><li><p>Markdown support</p></li><li><p>TypeScript support</p></li><li><p>I18n support (vi, en, zh, pt)</p></li><li><p>React support</p></li><li><p>Slash Commands</p></li><li><p>Multi Column</p></li><li><p>TailwindCss</p></li><li><p>Support emoji</p></li><li><p>Support iframe</p></li><li><p>Support mermaid</p></li></ul><h2>Installation</h2><pre><code class="language-bash">pnpm add reactjs-tiptap-editor</code></pre><p></p>`;
