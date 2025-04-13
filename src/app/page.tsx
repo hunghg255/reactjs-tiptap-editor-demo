ff69ba14a089af93792ae89bb5f0984e235e6af0
hunghg255 <giahunghust@gmail.com>
2025-04-14 00:20:36 +0700

    docs: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import Editor from "@/components/Editor/Editor";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import('@/components/Editor/Editor'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
